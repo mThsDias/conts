6c7996c1b3d9f6d5fab4d9cc5945fe4b1945a61e
matheusdiasmello@icloud.com <matheus@Matheus>
2023-05-16 16:28:43 -0300

    commit

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,8 +1,13 @@
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProject.module.css'
+
 function NewProject() {
     return (
-        <div>
-            <h1>Novo projeto</h1>
-        </div>
+        <section className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm btnText='Criar Projeto'/>
+        </section>
     )
 }
 
