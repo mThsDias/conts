fc53151a3e63456417441e3a15e28f960d8bcb3e
matheusdiasmello@icloud.com <matheus@Matheus>
2023-05-29 15:12:33 -0300

    first commit

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -8,14 +8,17 @@ import styles from './NavBar.module.css'
 function NavBar() {
     return (
         <nav className={styles.navBar}>
-                <Container>
-                    <Link to='/'><img src={logo} alt='Costs' /></Link>
-                    <ul className={styles.list}>
-                        <li className={styles.item}><Link to='/'>Home</Link></li>
-                        <li className={styles.item}><Link to='/projects'>Projetos</Link></li>
-                        <li className={styles.item}> <Link to='/contact'>Contato</Link></li>
-                        <li className={styles.item}><Link to='/company'>Empresa</Link></li>
-                    </ul>
+                <Container >
+                    <div className={styles.navBar_container}>
+                        <Link to='/'><img src={logo} alt='Costs' /></Link>
+                        <ul className={styles.list}>
+                            <li className={styles.item}><Link to='/'>Home</Link></li>
+                            <li className={styles.item}><Link to='/projects'>Projetos</Link></li>
+                            <li className={styles.item}> <Link to='/contact'>Contato</Link></li>
+                            <li className={styles.item}><Link to='/company'>Empresa</Link></li>
+                        </ul>
+                    </div>
+                    
                 </Container> 
         </nav>   
     )
