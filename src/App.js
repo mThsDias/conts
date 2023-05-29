333437f9761bd22315092cf622fb84c84c7adcbf
matheusdiasmello@icloud.com <matheus@Matheus>
2023-05-29 14:42:15 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
+import Projects from './components/pages/Projects'
+import Project from './components/pages/Project'
+
 import NavBar from './components/layout/NavBar'
 import Footer from './components/layout/Footer'
-import Projects from './components/pages/Projects'
-
 import Container from './components/layout/Container'
 
 function App() {
@@ -21,6 +23,7 @@ function App() {
                 <Route path='/company' element={<Company />}></Route>
                 <Route path='/newproject' element={<NewProject />}></Route>
                 <Route path='/projects' element={<Projects />}></Route>
+                <Route path='/project/:id' element={<Project />}></Route>
             </Routes>
         </Container>
         <Footer />
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -13,24 +13,24 @@ function NewProject() {
         // initialize cost and services
         project.cost = 0
         project.services = []
-
+        
         fetch("http://localhost:5000/projects",{
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-    
+            
             body: JSON.stringify(project),
-
-            }).then((resp) => resp.json())
-            .then((data) => {
+            
+        }).then((resp) => resp.json())
+        .then((data) => {
             console.log(data)
             //redirect
             navigate('/projects', { message: 'Projeto criado com sucesso!' })
-            })
-            .catch((err) => console.log(err))
+        })
+        .catch((err) => console.log(err))
     }
-
+    
     return (
         <section className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
